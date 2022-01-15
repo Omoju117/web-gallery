2538af5f428ac08b914bbd757c02b38e3f2c0d2c
kazukiendo <ka.endo@nagatake.co.jp>
2022-01-15 16:32:10 +0900

    configred image loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    loader: 'imgix',
+    path: 'https://omoju117.github.io/web-gallery/',
+  },
+};
