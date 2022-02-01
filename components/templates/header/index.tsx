8b5deb01027b341ca37e3472cc1864af703102ef
kazukiendo <ka.endo@nagatake.co.jp>
2022-02-01 17:59:13 +0900

    [update]added Article page that linked Notion

--- components/templates/header/index.tsx
+++ components/templates/header/index.tsx
@@ -2,6 +2,9 @@ import { VFC } from 'react';
 import style from './header.module.scss';
 import Link from 'next/link';
 
+const NOTION_ARTICLE_PAGE =
+  'https://forest-pufferfish-25b.notion.site/Article-Page-58eb5cd025ff409aa679db3f2021a8bb';
+
 const Header: VFC = () => {
   const debug = 'Header';
   return (
@@ -16,6 +19,15 @@ const Header: VFC = () => {
         </Link>
         <Link passHref href='/contact'>
           <a className={style.navigation_item}>Contact</a>
+        </Link>
+        <Link passHref href={NOTION_ARTICLE_PAGE}>
+          <a
+            className={style.navigation_item}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Article
+          </a>
         </Link>
       </div>
     </div>
