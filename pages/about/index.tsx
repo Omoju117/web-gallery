e314444bef8a9318ddc3874d27e9a4ad79ab52c8
kazukiendo <ka.endo@nagatake.co.jp>
2022-08-01 07:31:37 +0900

    update: About page's information

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -10,24 +10,20 @@ const About: VFC = () => {
       <section className={`${style.contents} ${style.fadeIn}`}>
         {/* <Logo /> */}
         <div className={style.contents_wrapper}>
-          <h1>Contact</h1>
+          <h1>Profile</h1>
           <p className={style.description}>
-            Hello, I’m Haley, an American illustrator from Rochester, New York
-            living and working in Koblenz, Germany. Clients: The New York Times,
-            The New Yorker, Trip Advisor, WeTransfer, House & Garden UK, The
-            Home Depot, Le Bon Marché, WIRED UK, WIRED Japan, Target, BBC,
-            Architektur & Wohnen, Brigitte, Bon Appétit, Epicurious, Thread For
-            Men, Society6, Psychologie Heute, Thread for Women, Overheard,
-            Eltern Magazin, Working Women Magazin, Haspa, Annabelle Magazin,
-            Freundin Magazin, Flow Magazine, Financial Times Weekend Magazine,
-            Myself Magazin, Condé Nast, VICE, ZEIT Wissen, Süddeutsche Zeitung
-            Magazin & more.{' '}
+            I’m Kazuki Endo, an Japanese illustrator and web developer, living
+            in Kanagawa . I do everything from web development to illustration
+            and painting. My work is a combination of things that are found in
+            daily life and changing perspectives. I use playful and comfortable
+            colors that feel people grace and unusually shaped objects to create
+            unique work for any situation or client.
           </p>
           <section className={style.profile}>
-            <h3>Profile</h3>
+            <h3>Information</h3>
             <p>Academic history: Seijo University</p>
             <p>Career: Web developer, Illustrator</p>
-            <p>TOEFL iBT Score: 95</p>
+            <p>TOEFL iBT Score: 90</p>
           </section>
         </div>
         <Footer />
